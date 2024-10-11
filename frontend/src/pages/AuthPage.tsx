97d07984bef6ae7b5955cf1965ed6c9ac48bb60d
Lígia Abreu <lilly.abreu20@gmail.com>
2024-10-11 20:39:05 -0300

    frontend of the authentication page (login/register) with notifications.

---
+++ frontend/src/pages/AuthPage.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './AuthPage.css';
+import LoginForm from '../components/loginForm';
+import SignupForm from '../components/singupForm';
+
+const AuthPage: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<'login' | 'signup'>('login');
+
+  const handleTabSwitch = (tab: 'login' | 'signup') => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div className="page">
+
+    <div className="auth-container">
+      <div className="tab-container">
+        <button
+          onClick={() => handleTabSwitch('login')}
+          className={`tab-button ${activeTab === 'login' ? 'active-tab' : 'inactive-tab'}`}
+          >
+          Login
+        </button>
+        <button
+          onClick={() => handleTabSwitch('signup')}
+          className={`tab-button ${activeTab === 'signup' ? 'active-tab' : 'inactive-tab'}`}
+          >
+          Cadastro
+        </button>
+      </div>
+      <div className="form-container">
+        {activeTab === 'login' ? <LoginForm /> : <SignupForm />}
+      </div>
+    </div>
+</div>
+  );
+};
+export default AuthPage;
