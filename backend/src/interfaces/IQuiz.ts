52d5abe467cf03880ae98fd88ac5956479fa91a1
delfo <carlos13bem@gmail.com>
2024-10-16 15:47:58 -0300

    feat: interfaces

---
+++ backend/src/interfaces/IQuiz.ts
@@ -0,0 +1,31 @@
+export interface IQuizQuestion {
+	id: number;
+	statement: string;
+	options: string[];
+	answer: string;
+	explanation: string;
+}
+
+export interface IQuiz {
+	id: string;
+	title: string;
+	theme: string;
+	number_of_question: number;
+	number_of_alternatives: number;
+	created_at?: string; //timestamp
+	questions: IQuizQuestion[];
+}
+
+// redis:
+export interface IVote {
+	id: string;
+	user_id: string;
+	poll_id: string;
+	poll_question_id: string;
+	user_choice: string;
+}
+
+export interface IPlayersPoll {
+	id: string;
+	users: [];
+}
---
+++ backend/src/websocket/index.ts
@@ -0,0 +1,51 @@
+import { IQuiz } from "../interfaces/IQuiz";
+import { QuizGenerator } from "../quiz-generator";
+import { ErrorWhileGeneratingQuiz } from "../utils/Exception";
+
+const quizGenerator = new QuizGenerator();
+let redisController: any;
+
+let ws: any;
+
+ws.on("postQuiz", async (e: any) => {
+	const { title, qntd_question, qntd_alternatives, theme } = e.body;
+	// validar
+	const prompt = quizGenerator.generatePrompt(
+		title,
+		theme,
+		qntd_question,
+		qntd_alternatives
+	);
+	const gptQuestions = await quizGenerator.create(prompt);
+	if (gptQuestions instanceof ErrorWhileGeneratingQuiz) {
+		return;
+	}
+	const gptQuestionsWithID = gptQuestions.map((question, index) => {
+		return {
+			id: index,
+			statement: question.statement,
+			options: question.options,
+			answer: question.answer,
+			explanation: question.explanation,
+		};
+	});
+	const quizID = generateUUID();
+	const quiz: IQuiz = {
+		id: quizID, //gerar id do quiz
+		title: title,
+		theme: theme,
+		number_of_alternatives: qntd_alternatives,
+		number_of_question: qntd_question,
+		questions: gptQuestionsWithID
+	}
+
+	redisController.set(quizID, quiz)
+	redisController.get(quizID)
+});
+
+ws.on("triggerStart")
+
+function generateUUID() {
+	return "Function not implemented.";
+}
+
