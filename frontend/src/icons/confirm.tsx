0b650321a7f3909c0609083b24c47199a23924ce
Pedrosavioo <pedrosaviolima618@gmail.com>
2024-10-18 13:28:48 -0300

    feat: add new icons (confirm and group)

---
+++ frontend/src/icons/confirm.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const ConfirmIcon = () => {
+	return (
+		<svg
+			width="20"
+			height="21"
+			viewBox="0 0 20 21"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<mask
+				id="mask0_279_265"
+				maskUnits="userSpaceOnUse"
+				x="0"
+				y="0"
+				width="20"
+				height="21"
+			>
+				<path
+					d="M1 10.5C1 5.53 5.03 1.5 10 1.5C14.97 1.5 19 5.53 19 10.5C19 15.47 14.97 19.5 10 19.5C5.03 19.5 1 15.47 1 10.5Z"
+					fill="white"
+					stroke="white"
+					strokeWidth="2"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+				/>
+				<path d="M6 10.5L9 13.5L14 8.5" fill="white" />
+				<path
+					d="M6 10.5L9 13.5L14 8.5"
+					stroke="black"
+					strokeWidth="2"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+				/>
+			</mask>
+			<g mask="url(#mask0_279_265)">
+				<rect x="-2" y="-1.5" width="24" height="24" fill="white" />
+			</g>
+		</svg>
+	);
+};
+
+export default ConfirmIcon;
