d1dfbd95cff60106ec08c8c79d6eda65ee53db73
Pedrosavioo <pedrosaviolima618@gmail.com>
2024-10-19 17:53:02 -0300

    feat: backend integration

--- frontend/src/pages/Statistic.tsx
+++ frontend/src/pages/Statistic.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Btn from "../components/button";
 import ExitIcon from "../icons/exit";
 import StarIcon from "../icons/star";
@@ -7,13 +7,52 @@ import PlayedPollsIcon from "../icons/playedPolls";
 import StarWinnerIcon from "../icons/starWinner";
 import "./css/Statistic.css";
 import { useNavigate } from "react-router-dom";
+import { UserAPI } from "../api/users";
+import { IUser } from "../interfaces/IUser";
+import MedalIcon from "../icons/medal";
 
 const Statistic = () => {
+	const [user, setUser] = useState<IUser | null>(null);
+	const userApi = new UserAPI();
+
+	// data
+	const date = new Date(user?.created_at as Date);
+	const months = [
+		"Janeiro",
+		"Fevereiro",
+		"Março",
+		"Abril",
+		"Maio",
+		"Junho",
+		"Julho",
+		"Agosto",
+		"Setembro",
+		"Outubro",
+		"Novembro",
+		"Dezembro",
+	];
+
 	const navigate = useNavigate();
 
 	function exit() {
-		navigate('/home')
+		navigate("/home");
 	}
+
+	useEffect(() => {
+		async function getUser() {
+			const resut = await userApi.getMyUser();
+			if (resut.statusCode === 200) {
+				setUser(resut.data);
+			}
+
+			if (resut.error) {
+				console.log("Erro ao buscar informações do usuário");
+				console.log("Erro:", resut);
+			}
+		}
+
+		getUser();
+	}, []);
 
 	return (
 		<section id="statistic-section">
@@ -24,29 +63,47 @@ const Statistic = () => {
 				className="btn-exit"
 				onClick={exit}
 			/>
-			<div id="img-profile-avatar"></div>
-			<div id="content-statistic">
-				<h2>Name user</h2>
-				<article id="user-statistic">
-					<section className="section-user-statistic">
-						<TrophyIcon />
-						<h3>Vitórias</h3>
-						<p>5</p>
-					</section>
-					<hr />
-					<section className="section-user-statistic">
-						<StarIcon />
-						<h3>Pontos</h3>
-						<p>5</p>
-					</section>
-					<hr />
-					<section className="section-user-statistic">
-						<PlayedPollsIcon />
-						<h3>Partidas</h3>
-						<p>5</p>
-					</section>
-				</article>
-				<article id="recent-quiz">
+			{user ? (
+				/* Foto de perfil */
+				/* <div id="img-profile-avatar"></div> */
+				<div id="content-statistic">
+					<h2>{user.name}</h2>
+					<p>
+						Por aqui desde {date.getDate()} de{" "}
+						{months[date.getMonth()]} de {date.getFullYear()}
+					</p>
+					<p>
+						Porcentagem de vitória{" "}
+						{user.played_polls === 0
+							? "0%"
+							: ((user.wins / user.played_polls) * 100).toFixed(
+									2
+							  ) + "%"}
+					</p>
+					<article id="user-statistic">
+						<section className="section-user-statistic">
+							<TrophyIcon />
+							<h3>Vitórias</h3>
+							<p>{user.wins}</p>
+						</section>
+						<section className="section-user-statistic">
+							<StarIcon />
+							<h3>Pontos</h3>
+							<p>{user.points}</p>
+						</section>
+						<section className="section-user-statistic">
+							<PlayedPollsIcon />
+							<h3>Partidas</h3>
+							<p>{user.played_polls}</p>
+						</section>
+						<section className="section-user-statistic">
+							<MedalIcon />
+							<h3>Medalhas</h3>
+							<p>{user.medals}</p>
+						</section>
+					</article>
+					{/* Quizzes Jogados Recentemente */}
+					{/* <article id="recent-quiz">
 					<h4>Jogado Recentemente</h4>
 					<ol id="div-played">
 						<li>
@@ -85,8 +142,11 @@ const Statistic = () => {
 							<StarWinnerIcon winner />
 						</li>
 					</ol>
-				</article>
-			</div>
+				</article> */}
+				</div>
+			) : (
+				<p>carregando...</p>
+			)}
 		</section>
 	);
 };
