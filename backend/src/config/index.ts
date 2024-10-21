35ba99604d428fb5af57527b9998439e4086795b
delfo <carlos13bem@gmail.com>
2024-10-21 17:40:12 -0300

    fix: redis & postgres now has its own host variable in .env

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -12,8 +12,9 @@ const dotenvIsConfig =
 	process.env.DB_DATABASE_NAME != null &&
 	process.env.DB_PASSWORD != null &&
 	process.env.DB_PORT != null &&
-	process.env.REDIS_PORT != null &&
 	process.env.DATABASE_URL != null &&
+	process.env.REDIS_HOST != null &&
+	process.env.REDIS_PORT != null &&
 	process.env.JWT != null &&
 	process.env.PAUSE_TIME_BETWEEN_QUESTIONS != null;
 
@@ -33,12 +34,14 @@ export const config = {
 	CLIENT_URL: process.env.CLIENT_URL || "http:localhost:3000",
 
 	DB_USER: process.env.DB_USER || "postgres",
-	DB_HOST: process.env.DB_HOST || "localhost",
+	DB_HOST: process.env.DB_HOST || "postgres",
 	DB_DATABASE_NAME: process.env.DB_DATABASE_NAME || "db",
 	DB_PASSWORD: process.env.DB_PASSWORD || "1234",
 	DB_PORT: process.env.DB_PORT || "5432",
-	REDIS_PORT: process.env.REDIS_PORT || 6379,
 	DATABASE_URL: process.env.DATABASE_URL || "",
+
+	REDIS_HOST: (process.env.REDIS_HOST as string | undefined) || "redis",
+	REDIS_PORT: (process.env.REDIS_PORT as number | undefined) || 6379,
 	JWT: process.env.JWT || "1234",
 
 	PAUSE_TIME_BETWEEN_QUESTIONS:
