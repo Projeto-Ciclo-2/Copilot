d529f37de6ec7a2fc2ceb8365b08c5e55e822ff9
Pedrosavioo <pedrosaviolima618@gmail.com>
2024-10-22 01:32:32 -0300

    fix: remove question current provider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { QuestionCurrentProvider } from "./context/questionCurrentContext";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
-root.render(
-	<QuestionCurrentProvider>
-		<App />
-	</QuestionCurrentProvider>
-);
+root.render(<App />);
